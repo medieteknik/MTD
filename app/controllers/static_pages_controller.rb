b11d7badb0cb6d6a4adabe222162fdd7d66d2186
Klas Eskilson <klas.eskilson@gmail.com>
2014-11-11 10:32:38 +0100

    Improve static_pages structure
    This affects both routes and the static pages controller. Some
    routes are translated.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home
+  def show
+    render "static_pages/#{params[:page]}"
+  end
+
+  # make sure we catch that nasty missing template-error
+  rescue_from ActionView::MissingTemplate do |exception|
+    redirect_to home_path, :flash => { :error => "not found" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   localized do
     devise_for :admins, :path => 'admin/user'
-    get '/' => 'static_pages#home', :as => :home
+    get '/' => 'static_pages#show', :as => :home, :page => 'home'
+
+    # in order to have translated routes, these are needed
+    get '/sponsorship' => 'static_pages#show', :page => 'sponsorship'
+    # get '/sponsorship/gold' => 'static_pages#show', :page => 'sponsorship/gold'
+    # get '/sponsorship/silver' => 'static_pages#show', :page => 'sponsorship/silver'
+    # get '/sponsorship/bronze' => 'static_pages#show', :page => 'sponsorship/bronze'
+
+    get '*page', to: 'static_pages#show'
   end
 
-  root to: 'static_pages#home'
+  root to: 'static_pages#show', :page => 'home'
 end
