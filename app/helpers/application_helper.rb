5751c4d4ea6417a33e1165e836f6118012a4afe0
Klas Eskilson <klas.eskilson@gmail.com>
2014-11-19 16:47:11 +0100

    Add meta tags, description helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,9 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
   def body_class(the_class)
     content_for(:body_class) { the_class }
   end
