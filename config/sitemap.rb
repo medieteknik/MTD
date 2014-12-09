ee6a137cb6eb225dcf3063a04f55aa9c0c95f197
Klas Eskilson <klas.eskilson@gmail.com>
2014-12-09 09:36:14 +0100

    Add temp sitemap, update gems
    Heroku have a read only file system, therefore we need to add
    these sitemaps until a more permanent solution is found.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,13 +1,11 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.medieteknikdagarna.se"
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemap/'
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
   # Links are added to the Sitemap in the order they are specified.
-  #
 
   group(:filename => :en) do
     add '/en', :changefreq => 'daily', :priority => 1
