74982111758df3c1413331977c19c9158a1242a8
Klas Eskilson <klas.eskilson@gmail.com>
2014-12-17 14:00:32 +0100

    Change assets version
    Possible fix for cached cors origin issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.0.1'
 
   # config html compression
   compressor_options = {
