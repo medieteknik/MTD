67332e5b89f29f9e9f7994287251bbb78ffaf0ff
Klas Eskilson <klas.eskilson@gmail.com>
2015-02-17 21:58:44 +0100

    Add remove functinality to host admin index

--- app/controllers/admin/hosts_controller.rb
+++ app/controllers/admin/hosts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HostsController < Admin::AdminController
   add_breadcrumb 'Hosts', :admin_hosts_path
 
-  # before_action :set_host, only: [:edit, :update, :destroy, :show]
+  before_action :set_host, only: [:destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
 
@@ -12,4 +12,18 @@ class Admin::HostsController < Admin::AdminController
   def positions
     @positions = HostPosition.order(:id).all
   end
+
+  def destroy
+    if @host.destroy
+      flash[:notice] = "Host deleted."
+    else
+      flash[:notice] = "Host could not be deleted. Contact webby!"
+    end
+    redirect_to admin_hosts_path
+  end
+
+  private
+    def set_host
+      @host = Host.find(params[:id])
+    end
 end
