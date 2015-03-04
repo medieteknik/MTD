6912a2671da77870bf90f8d01249097b41cb98e6
Klas Eskilson <klas.eskilson@gmail.com>
2015-03-04 21:44:56 +0100

    Add public company view

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,2 +1,13 @@
 module CompaniesHelper
+  def attendance(company)
+    return t('companies.show.attending.both') if company.first_day && company.second_day
+    return t('companies.show.attending.first') if company.first_day && !company.second_day
+    return t('companies.show.attending.second')
+  end
+
+  def sponsor(company)
+    return company.name + " " + t('companies.show.sponsor.gold') if company.sponsor == 'gold'
+    return t('companies.show.sponsor.fair') if company.sponsor == 'fair'
+    return company.name + " " + t('companies.show.sponsor.sponsor')
+  end
 end
