840a1b7e6e8ebd136f4f88fb734ad35715c88281
codingInSpace <jonathan.grangien@gmail.com>
2016-02-12 12:02:02 +0100

    Change graybar file ext to svg and set new config assets version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0.1'
+  config.assets.version = '1.0.2'
 
   # config html compression
   compressor_options = {
