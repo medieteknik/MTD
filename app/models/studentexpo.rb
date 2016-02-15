f5dbffd74e0ebc4834fe4fb98d69bd138ba81e19
codingInSpace <jonathan.grangien@gmail.com>
2016-02-15 01:45:39 +0100

    Make course code optional for student expo applications

--- app/models/studentexpo.rb
+++ app/models/studentexpo.rb
@@ -1,4 +1,10 @@
 class Studentexpo < ActiveRecord::Base
-  validates :title, :description, :course, :members, :name, :liuid, presence: true
+  before_save :set_course_title
+
+  validates :title, :description, :members, :name, :liuid, presence: true
   validates :liuid, format: { with: /([A-Za-z]{4,5}[0-9]{3})/ }
+
+	def set_course_title
+		self.course = "N/A" if self.course.blank?
+	end
 end
