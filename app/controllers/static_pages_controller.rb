25d5ad26e7bac7e52ad58dfc4d3a335714e1b6bc
codingInSpace <jonathan.grangien@gmail.com>
2016-03-24 15:36:07 +0100

    Update home view layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   end
 
   def home
-    @news = News.where('status = :status AND published_at <= :right_now', {status: 'public', right_now: DateTime.now}).order(published_at: :desc).limit(6)
+    @news = News.where('status = :status AND published_at <= :right_now', {status: 'public', right_now: DateTime.now}).order(published_at: :desc).limit(4)
     @gold_sponsors = Company.where(sponsor: 'gold', published: true).shuffle
     @silver_sponsors = Company.where(sponsor: 'silver', published: true).shuffle
   end
