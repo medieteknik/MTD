115b581644a074e013c98848506bd848f2f0dbaa
codingInSpace <jonathan.grangien@gmail.com>
2016-03-26 11:59:32 +0100

    Update and apply lectures schedule

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -6,10 +6,10 @@ class LecturesController < ApplicationController
   end
 
   def index
-    #@lectures_one = Lecture.all.where(published: true,
-      #start: (Date.new(2016, 4, 6).beginning_of_day)..(Date.new(2016, 4, 6).end_of_day)).order('start asc')
-    #@lectures_two = Lecture.all.where(published: true,
-      #start: (Date.new(2016, 4, 7).beginning_of_day)..(Date.new(2016, 4, 7).end_of_day)).order('start asc')
-    @lectures = Lecture.all.where(published: true)
+		@lectures_one = Lecture.all.where(published: true,
+			start: (Date.new(2016, 4, 6).beginning_of_day)..(Date.new(2016, 4, 6).end_of_day)).order('start asc')
+		@lectures_two = Lecture.all.where(published: true,
+			start: (Date.new(2016, 4, 7).beginning_of_day)..(Date.new(2016, 4, 7).end_of_day)).order('start asc')
+    #@lectures = Lecture.all.where(published: true)
   end
 end
