c46262259baf92ad2502c50604489ad6342adb0a
Jesper Lund <jeslu246@student.liu.se>
2018-01-19 18:24:01 +0100

    Heroku config

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -7,9 +7,9 @@ class LecturesController < ApplicationController
 
   def index
 		@lectures_one = Lecture.all.where(published: true,
-			start: (Date.new(2017, 03, 29).beginning_of_day)..(Date.new(2017, 03, 29).end_of_day)).order('start asc')
+			start: (Date.new(2018, 03, 21).beginning_of_day)..(Date.new(2018, 03, 21).end_of_day)).order('start asc')
 		@lectures_two = Lecture.all.where(published: true,
-			start: (Date.new(2017, 03, 30).beginning_of_day)..(Date.new(2017, 03, 30).end_of_day)).order('start asc')
+			start: (Date.new(2018, 03, 22).beginning_of_day)..(Date.new(2018, 03, 22).end_of_day)).order('start asc')
     #@lectures = Lecture.all.where(published: true)
   end
 end
